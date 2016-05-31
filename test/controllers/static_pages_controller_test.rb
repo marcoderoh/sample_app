8b48e451315eaeb52afe51a2c1a22bbf056d5997
marcoderoh <marcoderoh@gmail.com>
2016-05-31 16:25:06 +0300

    added user validations

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -40,4 +40,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 end
 
+
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,11 +6,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # end
   test "layout links" do 
     get root_path
+    # assert_select "a[href=?]",  signup_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
 
   end 
 end
