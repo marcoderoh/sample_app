b0ef82cc12234da6f8ac4684a77aee36c02864b6
marcoderoh <marcoderoh@gmail.com>
2016-06-03 13:05:29 +0300

    added sign up form and alerts to show successful and unsuccessful signups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,19 @@ class UsersController < ApplicationController
 
 
   def new
+  	@user = User.new
+  end
+  def create 
+  	@user =User.new(user_params)
+  	if @user.save
+  		flash[:success] = "welcome to the sample app manigga!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+  private
+  def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
