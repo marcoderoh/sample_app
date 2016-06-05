37015eec7335ae2319b5e803f502f73c9c915e0b
marcoderoh <marcoderoh@gmail.com>
2016-06-05 23:39:31 +0300

    added methods to create sessions so users can login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,9 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		#log in the user and redirect to the users' show page
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
   	else
   		#create an error msg
   	    flash.now[:danger] = 'Invalid email/password' 
@@ -15,5 +18,7 @@ class SessionsController < ApplicationController
   end 
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
