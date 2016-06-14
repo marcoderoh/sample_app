feb8d107da987eadb2c807b1332a509cc539bba9
marcoderoh <marcoderoh@gmail.com>
2016-06-14 09:02:31 +0300

    removed errors in user edit test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,15 @@ def current_user
     end
 end
 
+def redirect_back_or(default)
+  redirect_to(session[:forwarding_url] || default)
+  session.delete(:forwarding_url)
+end
+
+def store_location
+  session[:forwarding_url] = request.url if request.get?
+end
+
 def  logged_in?
 	!current_user.nil?
 end
